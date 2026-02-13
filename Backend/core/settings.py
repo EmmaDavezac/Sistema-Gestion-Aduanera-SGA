591c0e5069621ffbae25a59c064c24c1a8d22687
Emmanuel Davezac <lucianodavezac@gmail.com>
2026-02-13 11:23:34 -0300

    allowed hosts updated

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -6,7 +6,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-cambiame-en-produccion'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'sistema-gestion-aduanera-sga-1.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
