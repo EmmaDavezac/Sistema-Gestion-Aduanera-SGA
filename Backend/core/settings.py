3061d0f3a7fd29c98f60efe449da7d6399cca7b9
Emmanuel Davezac <lucianodavezac@gmail.com>
2026-02-13 12:01:38 -0300

    a

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cambiame-en-produccion'
 DEBUG = True
 ALLOWED_HOSTS = [
-    'sistema-gestion-aduanera-sga-1.onrender.com',
+    'https://sistema-gestion-aduanera-sga-1.onrender.com',
     'localhost',
     '127.0.0.1'
 ]
